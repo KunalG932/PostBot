d610e14c1f60113fe9e7342ca39b72659a8ea69b
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-02-18 11:57:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys  # Add this line to import sys
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from pymongo import MongoClient
+from handlers.echo import echo_router
+from handlers.start import start_router
 
 # Set your Telegram bot token here
 API_TOKEN = '6753603405:AAEXkgfWXPiBr_TGynYIpyCEwEeDg-Ax_Ec'
@@ -21,24 +23,16 @@ mongo_client = MongoClient(MONGO_URI)
 db = mongo_client[MONGO_DB]
 user_collection = db[MONGO_COLLECTION]
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
-dp.bot = bot
-
-# Enable logging
-logging.basicConfig(level=logging.INFO)
-
 
 # Command to show stats
-@dp.message_handler(commands=['stats'])
+@dp.message(commands=['stats'])
 async def show_stats(message: types.Message):
     total_users = user_collection.count_documents({})
     await message.reply(f"Total users: {total_users}")
 
 
 # Message handler for any text message
-@dp.message_handler(content_types=types.ContentType.TEXT)
+@dp.message(content_types=types.ContentType.TEXT)
 async def handle_text(message: types.Message):
     user_id = message.from_user.id
     user_collection.update_one({'_id': user_id}, {'$setOnInsert': {'_id': user_id}}, upsert=True)
@@ -52,12 +46,21 @@ async def on_startup(dp):
     await bot.send_message(chat_id=CHANNEL_ID, text="Bot is now alive!")
 
 
-# Set the on_startup event handler
 async def main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # Register all the routers from handlers package
+    dp.include_routers(
+        start_router,
+        echo_router,
+    )
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
     await dp.start_polling(bot, skip_updates=True)
 
 
 if __name__ == "__main__":
-    dp.register_on_startup(on_startup)
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
