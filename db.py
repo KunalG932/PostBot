b29b3e22b4e3f0c3b1d4a88e806dcd711dd40218
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-02-19 15:38:49 +0600

    Delete db.py

--- db.py
+++
@@ -1,6 +0,0 @@
-import motor.motor_asyncio
-
-# Replace the following with your MongoDB connection string
-MONGO_URI = "mongodb+srv://exp69:exp69@cluster0.kr93qbe.mongodb.net/?retryWrites=true&w=majority"
-mongo_client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-db = mongo_client["Postbot"]
