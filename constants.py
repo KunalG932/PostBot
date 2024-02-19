2b87f0777ee5687f58a7b35f2dc15bf0fe440821
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-02-19 18:12:07 +0600

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,3 @@
+TOKEN = "6753603405:AAEXkgfWXPiBr_TGynYIpyCEwEeDg-Ax_Ec"
+CHANNEL_ID = -1001824676870
+MONGO_URI = "mongodb+srv://exp69:exp69@cluster0.kr93qbe.mongodb.net/?retryWrites=true&w=majority"
