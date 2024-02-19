52c0ff18effdc2ae3892e7371cb12c3b87034012
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-02-19 18:29:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def cmd_create_post(message: types.Message):
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Text"), KeyboardButton(text="Media")],
-            [KeyboardButton(text="Back")]
+            [KeyboardButton(text="🔙 Back")]
         ],
         resize_keyboard=True,
     )
