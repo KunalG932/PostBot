cadb2fa95a7ab6ef7203da27409ba11e528c647c
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-02-19 14:50:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import asyncio
 import logging
 import motor.motor_asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command, ChatMemberUpdated
+from aiogram.filters import Command
+from aiogram.types.chat_member_updated import ChatMemberUpdated
 
 TOKEN = "6753603405:AAEXkgfWXPiBr_TGynYIpyCEwEeDg-Ax_Ec"
 CHANNEL_ID = -1001824676870
