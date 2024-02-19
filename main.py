f1af5edeec1ba233e37d23b67fe0db08cb5c59a7
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-02-19 12:42:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from os import getenv
 
 from handlers.start import start_router
 
-from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties  # Add this import
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ParseMode, DefaultBotProperties
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = "6753603405:AAEXkgfWXPiBr_TGynYIpyCEwEeDg-Ax_Ec"
 CHANNEL_ID = -1001824676870
+STICKER_ID = "CAACAgUAAxkBAAEIJvtl0vgM0tyELaA8FTFhF6-NDsoapwACNw0AAuyA0VV8CQZNPF5LHzQE"
 
 async def main() -> None:
     # Dispatcher is a root router
@@ -26,7 +26,10 @@ async def main() -> None:
     # Start polling
     await dp.start_polling(bot)
 
-    # Send a message to the specified channel indicating that the bot is now alive
+    # Send a sticker to the specified channel
+    await bot.send_sticker(chat_id=CHANNEL_ID, sticker=STICKER_ID)
+
+    # Send a text message to the specified channel indicating that the bot is now alive
     await bot.send_message(chat_id=CHANNEL_ID, text="Bot is now alive!", parse_mode=ParseMode.HTML)
 
 
