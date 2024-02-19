b69dffa54e195e1a7ed4ad894abce5d4177c0260
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-02-19 18:12:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-
-TOKEN = "6753603405:AAEXkgfWXPiBr_TGynYIpyCEwEeDg-Ax_Ec"
-CHANNEL_ID = -1001824676870
-MONGO_URI = "mongodb+srv://exp69:exp69@cluster0.kr93qbe.mongodb.net/?retryWrites=true&w=majority"
+from constants import *
 
 # Set the event loop policy to uvloop
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
