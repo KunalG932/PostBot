ed1ed7a76bbc264007dfb71657ed2b499a83a329
KunalG932 <23560086.scs@dypvp.edu.in>
2025-12-03 23:24:40 +0530

    fix: escape html in text input preview

--- handlers/text_input.py
+++ handlers/text_input.py
@@ -1,6 +1,7 @@
 """
 Text input processing
 """
+import html
 from aiogram import types
 from aiogram.enums import ParseMode
 
@@ -19,10 +20,15 @@ async def process_text_input(message: types.Message):
     user_data["state"] = "main_post_menu"
     set_user_data(message.from_user.id, user_data)
     
+    # Escape text for safe preview
+    preview_text = html.escape(message.text[:100])
+    if len(message.text) > 100:
+        preview_text += "..."
+    
     await message.answer(
-        f"**Text Added!**\n\n"
-        f"Preview: {message.text[:100]}{'...' if len(message.text) > 100 else ''}",
-        parse_mode=ParseMode.MARKDOWN
+        f"<b>Text Added!</b>\n\n"
+        f"Preview: {preview_text}",
+        parse_mode=ParseMode.HTML
     )
     
     from .post_menu import show_post_menu
